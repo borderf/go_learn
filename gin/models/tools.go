b0e3806774055a71949d23ead063d52a8319075f
borderf <1062548695@qq.com>
2022-08-18 08:08:52 +0800

    feature: upload file fix

--- gin/models/tools.go
+++ gin/models/tools.go
@@ -6,3 +6,12 @@ func UnixToTime(timestamp int) string {
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-02 15:04:05")
 }
+
+func GetDay() string {
+	template := "20060102"
+	return time.Now().Format(template)
+}
+
+func GetUnix() int64 {
+	return time.Now().Unix()
+}
--- gin/uploadFile/main.go
+++ gin/uploadFile/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"go_learn/gin/models"
 	"net/http"
+	"os"
 	"path"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,15 +21,42 @@ func main() {
 		})
 		adminRouter.POST("/user/doUpload", func(ctx *gin.Context) {
 			username := ctx.PostForm("username")
+			// 1、获取上传的文件
 			file, err := ctx.FormFile("face")
 			if err != nil {
-				ctx.JSON(http.StatusBadRequest, gin.H{
+				ctx.JSON(http.StatusOK, gin.H{
 					"success": false,
 					"msg":     "文件上传失败",
 				})
 			}
-			// 文件名称
-			dst := path.Join("./static/upload", file.Filename)
+			// 2、文件名称，是否合法 jpg png jpeg gif
+			extName := path.Ext(file.Filename)
+			allowExtMap := map[string]bool{
+				".jpg":  true,
+				".png":  true,
+				".jpeg": true,
+				".gif":  true,
+			}
+
+			if _, ok := allowExtMap[extName]; !ok {
+				ctx.JSON(http.StatusOK, gin.H{
+					"msg": "上传文件格式不合法",
+				})
+			}
+			// 3、创建图片保存目录
+			day := models.GetDay()
+			dir := path.Join("./static/upload", day)
+			err = os.MkdirAll(dir, 0666)
+			if err != nil {
+				ctx.JSON(http.StatusOK, gin.H{
+					"msg": "创建文件目录失败",
+				})
+			}
+			// 4、生成文件和文件保存的目录
+			unix := models.GetUnix()
+			filename := strconv.FormatInt(unix, 10) + extName
+			dst := path.Join(dir, filename)
+			// 5、执行上传
 			ctx.SaveUploadedFile(file, dst)
 			ctx.JSON(http.StatusOK, gin.H{
 				"success":  true,
